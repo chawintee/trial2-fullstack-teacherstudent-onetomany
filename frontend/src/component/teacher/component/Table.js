009eedbb65dca9d19c3fd3eef467f3646ffc1487
Chawintee <Chawinteetea@gmail.com>
2020-04-29 00:00:24 +0700

    update select

--- frontend/src/component/teacher/component/Table.js
+++ frontend/src/component/teacher/component/Table.js
@@ -3,10 +3,11 @@ import React from 'react'
 function Table(props) {
 
     const thisEle = props.data.filter(item=> item.year == props.ele)
+    const thisEle1 = thisEle.sort((a,b)=> a.number - b.number)
 
     return (
         <div>
-            {thisEle.map(ele=> 
+            {thisEle1.map(ele=> 
             <tr>
                 <td>{ele.number}</td>
                 <td>{ele.name}</td>
--- frontend/src/component/teacher/tShow.js
+++ frontend/src/component/teacher/tShow.js
@@ -1,41 +1,69 @@
-import React,{useState ,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from '../../config/axios'
 import Year from './component/Year';
 
 function TShow() {
 
     const [showListStudent, setShowListStudent] = useState([]);
+    const [optionInSelect, setOptionInSelect] = useState([]);
 
-    const fetchData = async() => {
+
+    const fetchData = async () => {
         const result = await axios.get('/student');
         await result.data.map(ele => {
-            ele.editStatus = false ;
+            ele.editStatus = false;
             ele.showStatus = false;
-            }) //add state to obj
+        }) //add state to obj
         const data = await result.data;
         await setShowListStudent(data);
+        const onlyOptionToSelect = await showListStudent.map(ele=>ele.year);
+        const optionInSelect= await [...new Set(onlyOptionToSelect)];
+        await setOptionInSelect(optionInSelect)
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchData()
-    },[])
+    }, [])
 
-
-    const logData = () => {
-        console.log(showListStudent);
-    }
 
     
+    const optionToSelect = async () => {
+        console.log("Hello")
+        const onlyOptionToSelect = await showListStudent.map(ele=>ele.year);
+        // console.log(onlyOptionToSelect)
+        const optionInSelect= await [...new Set(onlyOptionToSelect)];
+        // console.log(optionInSelect)
+        setOptionInSelect(optionInSelect)
+
+    }
+
+    const selectOption = (selected) => {
+        console.log("Hello")
+        console.log(selected)
+    }
+
+
+    
+    
+    const logData = () => {
+        console.log(showListStudent);
+        // console.log(onlyOptionToSelect);
+        // console.log(optionInSelect);
+    }
+
     return (
         <div>
             Student Show for teacher
+            <select id="Year" onMouseOver={optionToSelect}>
+                {optionInSelect.map(ele=> <option value={ele} onClick={selectOption({ele})}>{ele}</option> )}
+            </select>
+
             <ul>
                 <Year data={showListStudent} />
-              
             </ul>
 
             <button onClick={logData}>log</button>
-           
+
         </div>
     )
 }
