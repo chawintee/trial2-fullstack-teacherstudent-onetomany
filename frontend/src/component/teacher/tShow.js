b602b42d93e917c503618073b0418907c7d5e07e
Chawintee <Chawinteetea@gmail.com>
2020-05-01 21:38:06 +0700

    add select edit not render to all but delay 1 step

--- frontend/src/component/teacher/tShow.js
+++ frontend/src/component/teacher/tShow.js
@@ -7,8 +7,8 @@ function TShow() {
     const [showListStudent, setShowListStudent] = useState([]);
     const [optionInSelect, setOptionInSelect] = useState([]);
     const [showSelected, setShowSelected] = useState([]);
-    const [editText,setEditText] = useState("");
-    const [selectStatus,setSelectStatus] = useState("All");
+    const [editText, setEditText] = useState("");
+    const [selectStatus, setSelectStatus] = useState("All");
 
 
     //fetch Data
@@ -24,9 +24,9 @@ function TShow() {
         setShowSelected(showListStudent)
     }
 
-    
+
     useEffect(() => {
-        fetchData() 
+        fetchData()
     }, [])
     useEffect(() => {
         optionToSelect()
@@ -34,49 +34,58 @@ function TShow() {
     useEffect(() => {
         setShowSelected(showListStudent)
     }, [optionInSelect])
-    
-    
-    
+
+
+
     //Select
-    
+
     const optionToSelect = async () => {
         // console.log("Hello")
-        const onlyOptionToSelect = showListStudent.map(ele=>ele.year);
+        const onlyOptionToSelect = showListStudent.map(ele => ele.year);
         // console.log(onlyOptionToSelect)
-        const optionInSelect= await [...new Set(onlyOptionToSelect)];
+        const optionInSelect = await [...new Set(onlyOptionToSelect)];
         optionInSelect.sort()
         // console.log(optionInSelect)
-        const optionInSelect1 = await optionInSelect.map((ele,index)=>({id:index+1 ,year:ele}));
-        const optionInSelectAddAll = [({id:0,year:"All"}),...optionInSelect1]
-        setOptionInSelect(optionInSelectAddAll)        
+        const optionInSelect1 = await optionInSelect.map((ele, index) => ({ id: index + 1, year: ele }));
+        const optionInSelectAddAll = [({ id: 0, year: "All" }), ...optionInSelect1]
+        setOptionInSelect(optionInSelectAddAll)
     }
-    
-    
-    
-    
+
+
+
+
     const selectOption = (e) => {
         // console.log("Hello")
         // console.log(e.target.value)
         setSelectStatus(e.target.value)
         selectOption1();
     }
-    
-    const selectOption1 = () => {
-        if(selectStatus == "All") {
-            setShowSelected(showListStudent);
-        }else{
-            const selected = showListStudent.filter((ele)=> ele.year == selectStatus);
+
+    const selectOption1 = async () => {
+        const result = await axios.get('/student');
+        // console.log(result.data);
+        const data = result.data.map(ele => ({
+            ...ele,
+            editStatus: false,
+            showStatus: false,
+        }))
+        if (selectStatus == "All") {
+            // fetchData()
+            setShowListStudent(data);
+            setShowSelected(showListStudent)
+        } else {
+            const selected = showListStudent.filter((ele) => ele.year == selectStatus);
             // console.log(s elected)
             setShowSelected(selected)
         }
         // console.log(selectStatus)
         // console.log(showSelected)
     }
-    
+
     useEffect(() => {
         selectOption1()
     }, [selectStatus])
-    
+
 
 
 
@@ -85,7 +94,7 @@ function TShow() {
 
 
     //Log
-    
+
     const logData = () => {
         console.log(showListStudent);
         // console.log(onlyOptionToSelect);
@@ -105,11 +114,11 @@ function TShow() {
             editStatus: false,
             showStatus: false,
         }))
-        if(selectStatus == "All") {
+        if (selectStatus == "All") {
             setShowSelected(data);
-        }else{
-            const selected = data.filter((ele)=> ele.year == selectStatus);
-            // console.log(s elected)
+        } else {
+            const selected = data.filter((ele) => ele.year == selectStatus);
+            // console.log(selected)
             setShowSelected(selected)
         }
     }
@@ -117,53 +126,66 @@ function TShow() {
     //EditStatus
 
     const changeStatusToEdit = async (targetId) => {
-        const updateStatus = showListStudent.map((ele)=>{
-            if(ele.id == targetId){
-                return({...ele,editStatus: !ele.editStatus})
+        const updateStatus = showSelected.map((ele) => {
+            if (ele.id == targetId) {
+                return ({ ...ele, editStatus: !ele.editStatus })
             }
             return ele
         })
 
-        await setShowListStudent(updateStatus);
-        await setShowSelected(updateStatus);
-        console.log(targetId);
+        // await setShowListStudent(updateStatus);
+        setShowSelected(updateStatus);
+        // console.log(targetId);
+
     }
+
 
     const onChangeToEditText = (e) => {
         setEditText(e.target.value)
         console.log(editText)
     }
 
-    const setToEdit = async (targetId,text,editStatus) => {
+    const setToEdit = async (targetId, text, editStatus) => {
         // console.log(targetId,text,editStatus)
         // const a = text
         const body = {
-            [text] : editText
+            [text]: editText
         }
-        await axios.put(`/student/${targetId}`,body);
+        await axios.put(`/student/${targetId}`, body);
+
         const result = await axios.get(`/student`);
+
         const data = await result.data.map(ele => ({
             ...ele,
             editStatus: false,
             showStatus: false,
         }))
-        await setShowListStudent(data);
-        await setShowSelected(data);
+        if (selectStatus == "All") {
+            setShowListStudent(data);
+            setShowSelected(data);
+        } else {
+            setShowListStudent(data);
+            const selected = data.filter((ele) => ele.year == selectStatus);
+            // console.log(selected)
+            setSelectStatus(selectStatus)
+            setShowSelected(selected)
+        }
+        selectOption1();
+
         setEditText("")
     }
 
 
 
-
     return (
         <div>
-            Student Show for teacher<br/>
+            Student Show for teacher<br />
             <select id="Year" onChange={selectOption} defaultValue="All" >
-                {optionInSelect.map(ele=> <option key={ele.id} label={ele.year} value={ele.year} >{ele.year}</option> )}
+                {optionInSelect.map(ele => <option key={ele.id} label={ele.year} value={ele.year} >{ele.year}</option>)}
             </select>
 
             <ul>
-                <Year data={showSelected} onDelete={deleteEle} dbClickToEdit={changeStatusToEdit} editText={onChangeToEditText} editToText={editText} setToEdit={setToEdit}/>
+                <Year data={showSelected} onDelete={deleteEle} dbClickToEdit={changeStatusToEdit} editText={onChangeToEditText} editToText={editText} setToEdit={setToEdit} />
             </ul>
 
             <button onClick={logData}>log</button>
